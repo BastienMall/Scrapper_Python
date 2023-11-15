379f010beecd7c9d80492f83427aace3df8fb12d
BastienMall <bastien.mall@hotmail.fr>
2023-11-15 12:44:37 +0100

    adaptation au nouveau site

--- scrapper.py
+++ scrapper.py
@@ -4,29 +4,26 @@ import csv
 
 def scrape_page(soup, quotes):
 
-    quote_elements = soup.find_all('div', class_='quote')
+    quote_elements = soup.find_all('div', class_='paper_paper__1PY90 paper_outline__lwsUX card_card__lQWDv card_noPadding__D8PcU styles_wrapper__2JOo2')
 
     for quote_element in quote_elements:
 
-        text = quote_element.find('span', class_='text').text
+        title = quote_element.find('p', class_='typography_heading-xs__jSwUz typography_appearance-default__AAY17 styles_displayName__GOhL2').text
 
-        author = quote_element.find('small', class_='author').text
+        scoring = quote_element.find('p', class_='typography_body-m__xgxZ_ typography_appearance-subtle__8_H2l styles_ratingText__yQ5S7').text
 
-        tag_elements = quote_element.find('div', class_='tags').find_all('a', class_='tag')
+        domaine = quote_element.find('span', class_='typography_body-s__aY15Q typography_appearance-default__AAY17').text
 
-        tags = []
-        for tag_element in tag_elements:
-            tags.append(tag_element.text)
 
         quotes.append(
             {
-                'text': text,
-                'author': author,
-                'tags': ', '.join(tags) 
+                'Entreprise': title,
+                'Notation': scoring,
+                'Domaine': domaine
             }
         )
 
-url = 'https://quotes.toscrape.com'
+url = 'https://fr.trustpilot.com/categories/contractors_consultants'
 
 page = requests.get(url)
 
@@ -49,11 +46,11 @@ while next_li_element is not None:
 
     next_li_element = soup.find('li', class_='next')
 
-csv_file = open('quotes.csv', 'w', encoding='utf-8', newline='')
+csv_file = open('Scoring.csv', 'w', encoding='utf-8', newline='')
 
 writer = csv.writer(csv_file)
 
-writer.writerow(['Text', 'Author', 'Tags'])
+writer.writerow(['Entreprise', 'Notation', 'Domaine'])
 
 for quote in quotes:
     writer.writerow(quote.values())
